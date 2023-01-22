1402b0ed8e076f6df7f5f1ee08d171e58f92d90d
Filip Petersson <zmarfan@hotmail.com>
2023-01-22 13:25:50 +0100

    Created builder for GameSettings

--- src/Program.cs
+++ src/Program.cs
@@ -1,51 +1,48 @@
 ﻿using Worms.engine.core;
 using Worms.engine.core.audio;
 using Worms.engine.core.input.listener;
-using Worms.engine.scene;
 using Worms.game.scenes;
 
 namespace Worms;
 
 internal static class Program {
     private static void Main() {
-        List<InputListener> listeners = new() {
-            InputListenerBuilder.Builder("horizontal", Button.D)
-                .SetNegativeButton(Button.A)
-                .SetAltPositiveButton(Button.RIGHT)
-                .SetAltNegativeButton(Button.LEFT)
-                .SetSnap(false)
-                .Build(),
-            InputListenerBuilder.Builder("vertical", Button.W)
-                .SetNegativeButton(Button.S)
-                .SetAltPositiveButton(Button.UP)
-                .SetAltNegativeButton(Button.DOWN)
-                .SetAxis(InputAxis.Y_AXIS)
-                .Build(),
-            InputListenerBuilder.Builder("action", Button.RIGHT_MOUSE)
-                .SetAltPositiveButton(Button.MIDDLE_MOUSE)
-                .Build(),
-            InputListenerBuilder.Builder("explosion", Button.SPACE).Build(),
-            InputListenerBuilder.Builder("cameraZoom", Button.NUM_1)
-                .SetNegativeButton(Button.NUM_2)
-                .Build(),
-            InputListenerBuilder.Builder("animationTest1", Button.I).Build(),
-            InputListenerBuilder.Builder("animationTest2", Button.O).Build(),
-            InputListenerBuilder.Builder("animationTest3", Button.P).Build(),
-        };
-        
-        Game game = new(new GameSettings(
-            true, 
-            "test game",
-            1200, 
-            800,
-            new List<Scene> { Scene1.GetScene() },
-            listeners, 
-            new List<string> { "layer1", "layer2", "layer3" },
-            new AudioSettings(Volume.Max(), new List<AudioChannel> {
+        Game game = new(GameSettingsBuilder
+            .Builder(new AudioSettings(Volume.Max(), new List<AudioChannel> {
                 new("effects", Volume.Max()),
                 new("music", Volume.Max()),
-            })
-        ));
+            }))
+            .SetWindowWidth(1200)
+            .SetWindowHeight(800)
+            .AddScenes(Scene1.GetScene())
+            .AddInputListeners(
+                InputListenerBuilder.Builder("horizontal", Button.D)
+                    .SetNegativeButton(Button.A)
+                    .SetAltPositiveButton(Button.RIGHT)
+                    .SetAltNegativeButton(Button.LEFT)
+                    .SetSnap(false)
+                    .Build(),
+                InputListenerBuilder.Builder("vertical", Button.W)
+                    .SetNegativeButton(Button.S)
+                    .SetAltPositiveButton(Button.UP)
+                    .SetAltNegativeButton(Button.DOWN)
+                    .SetAxis(InputAxis.Y_AXIS)
+                    .Build(),
+                InputListenerBuilder.Builder("action", Button.RIGHT_MOUSE)
+                    .SetAltPositiveButton(Button.MIDDLE_MOUSE)
+                    .Build(),
+                InputListenerBuilder.Builder("explosion", Button.SPACE).Build(),
+                InputListenerBuilder.Builder("cameraZoom", Button.NUM_1)
+                    .SetNegativeButton(Button.NUM_2)
+                    .Build(),
+                InputListenerBuilder.Builder("animationTest1", Button.I).Build(),
+                InputListenerBuilder.Builder("animationTest2", Button.O).Build(),
+                InputListenerBuilder.Builder("animationTest3", Button.P).Build()
+            )
+            .AddSortLayers("layer1", "layer2", "layer3")
+            .Build()
+        );
+
         game.Run();
     }
 }
---
+++ src/engine/core/GameSettingsBuilder.cs
@@ -0,0 +1,76 @@
+﻿using Worms.engine.core.audio;
+using Worms.engine.core.input.listener;
+using Worms.engine.scene;
+
+namespace Worms.engine.core; 
+
+public class GameSettingsBuilder {
+    private bool _debug = true;
+    private string _title = "My Game";
+    private int _width = 600;
+    private int _height = 400;
+    private readonly List<Scene> _scenes = new();
+    private readonly List<InputListener> _inputListeners = new();
+    private readonly List<string> _sortLayers = new();
+    private readonly AudioSettings _audioSettings;
+
+    private GameSettingsBuilder(AudioSettings audioSettings) {
+        _audioSettings = audioSettings;
+    }
+
+    public static GameSettingsBuilder Builder(AudioSettings audioSettings) {
+        return new GameSettingsBuilder(audioSettings);
+    }
+
+    public GameSettings Build() {
+        return new GameSettings(_debug, _title, _width, _height, _scenes, _inputListeners, _sortLayers, _audioSettings);
+    }
+
+    public GameSettingsBuilder SetDebugMode() {
+        _debug = true;
+        return this;
+    } 
+    
+    public GameSettingsBuilder SetBuildMode() {
+        _debug = false;
+        return this;
+    } 
+        
+    public GameSettingsBuilder SetTitle(string title) {
+        _title = title;
+        return this;
+    } 
+    
+    public GameSettingsBuilder SetWindowWidth(int width) {
+        _width = width;
+        return this;
+    } 
+    
+    public GameSettingsBuilder SetWindowHeight(int height) {
+        _height = height;
+        return this;
+    }
+    
+    public GameSettingsBuilder AddScenes(params Scene[] scenes) {
+        foreach (Scene scene in scenes) {
+            _scenes.Add(scene);
+        }
+        return this;
+    }
+    
+    public GameSettingsBuilder AddInputListeners(params InputListener[] listeners) {
+        foreach (InputListener inputListener in listeners) {
+            _inputListeners.Add(inputListener);
+        }
+
+        return this;
+    }
+    
+    public GameSettingsBuilder AddSortLayers(params string[] layers) {
+        foreach (string layer in layers) {
+            _sortLayers.Add(layer);
+        }
+
+        return this;
+    }
+}
